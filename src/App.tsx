225ffbf4285d5fd7966b713b54cc2750e9d1a168
miten <tgoodarzi@promotal.com>
2023-03-09 21:56:50 +0100

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="w-3/12 flex-col flex-nowrap flex  gap-4">
+                {
+                    ['thomas', 'arnaud'].map(name =>
+                        <div className="shadow-md p-10 w-full bg-black gap-4">
+                            <p className="text-white">{name}</p>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default App;
