1e192a176881624aa8bb88d7cc2d1b14413ba968
miten <tgoodarzi@promotal.com>
2023-03-09 23:00:24 +0100

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
         <div className="App h-screen flex-col flex-wrap items-center p-10">
             <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4">
                 {
-                    ['thomas', 'arnaud', 'fabien', 'sophie', 'lucien', 'laura', 'nicolas', 'fabius'].map(name =>
-                        <div className="shadow-md p-10 w-full bg-black gap-4">
-                            <p className="text-3xl font-bold uppercase italic text-white">{name}</p>
+                    ['thomas', 'arnaud', 'fabien', 'sophie', 'lucien', 'laura', 'nicolas', 'fabius'].map((name, idx) =>
+                        <div className={`shadow-md p-10 w-full gap-4 ${idx%2 === 0 ? `bg-black` : `bg-white`}`}>
+                            <p className={`text-3xl font-bold uppercase italic ${idx%2 !== 0 ? `text-black` : `text-white`}`}>{name}</p>
                         </div>
                     )
                 }
